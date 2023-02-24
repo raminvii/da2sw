4a5a3db77cd7c0c6b699cd9a74c369e1eb342d4f
raminvii <105510476+raminvii@users.noreply.github.com>
2023-02-24 16:47:40 +0000

    Update Shop.py

--- Shop.py
+++ Shop.py
@@ -1,6 +1,3 @@
-
-import streamlit as st
-
 # Define a dictionary containing information about the products
 # Define a dictionary containing information about the products
 products = {
@@ -8,280 +5,280 @@ products = {
         "name": "T-Shirt1",
         "description": "A classic fit t-shirt with a ribbed neckline. Made of 100% cotton. Comes in sizes S-XXL.",
         "price": "$20.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\tshirt1.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\tshirt1.png",
         "recommended": ["T-Shirt2", "T-Shirt3", "T-Shirt4"]
     },
     "T-Shirt2": {
         "name": "T-Shirt2",
         "description": "A classic fit t-shirt with a ribbed neckline. Made of 100% cotton. Comes in sizes S-XXL.",
         "price": "$20.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\tshirt2.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\tshirt2.png",
         "recommended": ["T-Shirt1", "T-Shirt3", "T-Shirt4"]
     },
     "T-Shirt3": {
         "name": "T-Shirt3",
         "description": "A classic fit t-shirt with a ribbed neckline. Made of 100% cotton. Comes in sizes S-XXL.",
         "price": "$20.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\tshirt3.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\tshirt3.png",
         "recommended": ["T-Shirt1", "T-Shirt2", "T-Shirt4"]
     },
     "T-Shirt4": {
         "name": "T-Shirt4",
         "description": "A classic fit t-shirt with a ribbed neckline. Made of 100% cotton. Comes in sizes S-XXL.",
         "price": "$20.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\tshirt4.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\tshirt4.png",
         "recommended": ["T-Shirt1", "T-Shirt2", "T-Shirt3"]
     },
     "Trouser1": {
         "name": "Trouser1",
         "description": "A pair of comfortable trouser made of 100% denim. Comes in sizes 28-40",
         "price": "$35.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\trousers1.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\trousers1.png",
         "recommended": ["Trouser2", "Trouser3", "Trouser4"]
     },
     "Trouser2": {
         "name": "Trouser2",
         "description": "A pair of comfortable trouser made of 100% denim. Comes in sizes 28-40",
         "price": "$35.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\trousers2.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\trousers2.png",
         "recommended": ["Trouser1", "Trouser3", "Trouser4"]
     },
     "Trouser3": {
         "name": "Trouser3",
         "description": "A pair of comfortable trouser made of 100% denim. Comes in sizes 28-40",
         "price": "$35.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\trousers3.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\trousers3.png",
         "recommended": ["Trouser2", "Trouser4", "Trouser1"]
     },
     "Trouser4": {
         "name": "Trouser4",
         "description": "A pair of comfortable trouser made of 100% denim. Comes in sizes 28-40",
         "price": "$35.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\trousers4.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\trousers4.png",
         "recommended": ["Trouser1", "Trouser2", "Trouser3"]
     },
     "Pullover1": {
         "name": "Pullover1",
         "description": "A classic pullover with a modern twist. Made of a cotton and polyester blend. Comes in sizes XS-L.",
         "price": "$65.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\pullover1.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\pullover1.png",
         "recommended": ["Pullover4", "Pullover2", "Pullover3"]
     },
     "Pullover2": {
         "name": "Pullover2",
         "description": "A classic pullover with a modern twist. Made of a cotton and polyester blend. Comes in sizes XS-L.",
         "price": "$65.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\pullover2.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\pullover2.png",
         "recommended": ["Pullover1", "Pullover3", "Pullover4"]
     },
     "Pullover3": {
         "name": "Pullover3",
         "description": "A classic pullover with a modern twist. Made of a cotton and polyester blend. Comes in sizes XS-L.",
         "price": "$65.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\pullover3.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\pullover3.png",
         "recommended": ["Pullover1", "Pullover2", "Pullover4"]
     },
     "Pullover4": {
         "name": "Pullover4",
         "description": "A classic pullover with a modern twist. Made of a cotton and polyester blend. Comes in sizes XS-L.",
         "price": "$65.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\pullover4.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\pullover4.png",
         "recommended": ["Pullover1", "Pullover2", "Pullover3"]
     },
     "Dress1": {
         "name": "Dress1",
         "description": "A beautiful dress made of silk. Perfect for a night out. Comes in sizes XS-L.",
         "price": "$100.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\dress1.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\dress1.png",
         "recommended": ["Dress2", "Dress3", "Dress4"]
     },
     "Dress2": {
         "name": "Dress2",
         "description": "A beautiful dress made of silk. Perfect for a night out. Comes in sizes XS-L.",
         "price": "$100.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\dress2.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\dress2.png",
         "recommended": ["Dress1", "Dress3", "Dress4"]
     },
     "Dress3": {
         "name": "Dress3",
         "description": "A beautiful dress made of silk. Perfect for a night out. Comes in sizes XS-L.",
         "price": "$100.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\dress3.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\dress3.png",
         "recommended": ["Dress1", "Dress2", "Dress4"]
     },
     "Dress4": {
         "name": "Dress4",
         "description": "A beautiful dress made of silk. Perfect for a night out. Comes in sizes XS-L.",
         "price": "$100.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\dress4.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\dress4.png",
         "recommended": ["Dress1", "Dress2", "Dress3"]
     },
     "Coat1": {
         "name": "Coat1",
         "description": "Stay warm and stylish this winter with our cozy and fashionable coat. Comes in sizes S-XL.",
         "price": "$125.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\coat1.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\coat1.png",
         "recommended": ["Coat2", "Coat3", "Coat4"]
     },
     "Coat2": {
         "name": "Coat2",
         "description": "Stay warm and stylish this winter with our cozy and fashionable coat. Comes in sizes S-XL.",
         "price": "$125.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\coat2.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\coat2.png",
         "recommended": ["Coat1", "Coat3", "Coat4"]
     },
     "Coat3": {
         "name": "Coat3",
         "description": "Stay warm and stylish this winter with our cozy and fashionable coat. Comes in sizes S-XL.",
         "price": "$125.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\coat3.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\coat3.png",
         "recommended": ["Coat1", "Coat2", "Coat4"]
     },
     "Coat4": {
         "name": "Coat4",
         "description": "Stay warm and stylish this winter with our cozy and fashionable coat. Comes in sizes S-XL.",
         "price": "$125.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\coat4.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\coat4.png",
         "recommended": ["Coat1", "Coat2", "Coat3"]
     },
     "Sandal1": {
         "name": "Sandal1",
         "description": "Step into summer with our sleek and comfortable sandals.",
         "price": "$45.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\sandals1.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\sandals1.png",
         "recommended": ["Sandal2", "Sandal3", "Sandal4"]
     },
     "Sandal2": {
         "name": "Sandal2",
         "description": "Step into summer with our sleek and comfortable sandals.",
         "price": "$45.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\sandals2.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\sandals2.png",
         "recommended": ["Sandal1", "Sandal3", "Sandal4"]
     },
     "Sandal3": {
         "name": "Sandal3",
         "description": "Step into summer with our sleek and comfortable sandals.",
         "price": "$45.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\sandals3.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\sandals3.png",
         "recommended": ["Sandal1", "Sandal2", "Sandal4"]
     },
     "Sandal4": {
         "name": "Sandal4",
         "description": "Step into summer with our sleek and comfortable sandals.",
         "price": "$45.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\sandals4.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\sandals4.png",
         "recommended": ["Sandal1", "Sandal2", "Sandal3"]
     },
     "Shirt1": {
         "name": "Shirt1",
         "description": "Elevate your wardrobe with our versatile and comfortable shirt. Comes in sizes S-XL.",
         "price": "$60.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\shirt1.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\shirt1.png",
         "recommended": ["Shirt2", "Shirt3", "Shirt4"]
     },
     "Shirt2": {
         "name": "Shirt2",
         "description": "Elevate your wardrobe with our versatile and comfortable shirt. Comes in sizes S-XL.",
         "price": "$60.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\shirt2.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\shirt2.png",
         "recommended": ["Shirt1", "Shirt3", "Shirt4"]
     },
     "Shirt3": {
         "name": "Shirt3",
         "description": "Elevate your wardrobe with our versatile and comfortable shirt. Comes in sizes S-XL.",
         "price": "$60.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\shirt3.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\shirt3.png",
         "recommended": ["Shirt1", "Shirt2", "Shirt4"]
     },
     "Shirt4": {
         "name": "Shirt4",
         "description": "Elevate your wardrobe with our versatile and comfortable shirt. Comes in sizes S-XL.",
         "price": "$60.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\shirt4.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\shirt4.png",
         "recommended": ["Shirt1", "Shirt2", "Shirt3"]
     },
     "Sneaker1": {
         "name": "Sneaker1",
         "description": "Experience the perfect combination of style and comfort with our trendy and supportive sneaker.",
         "price": "$90.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\sneaker1.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\sneaker1.png",
         "recommended": ["Sneaker2", "Sneaker3", "Sneaker4"]
     },
     "Sneaker2": {
         "name": "Sneaker2",
         "description": "Experience the perfect combination of style and comfort with our trendy and supportive sneaker.",
         "price": "$90.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\sneaker2.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\sneaker2.png",
         "recommended": ["Sneaker1", "Sneaker3", "Sneaker4"]
     },
     "Sneaker3": {
         "name": "Sneaker3",
         "description": "Experience the perfect combination of style and comfort with our trendy and supportive sneaker.",
         "price": "$90.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\sneaker3.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\sneaker3.png",
         "recommended": ["Sneaker1", "Sneaker2", "Sneaker4"]
     },
     "Sneaker4": {
         "name": "Sneaker4",
         "description": "Experience the perfect combination of style and comfort with our trendy and supportive sneaker.",
         "price": "$90.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\sneaker4.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\sneaker4.png",
         "recommended": ["Sneaker1", "Sneaker2", "Sneaker3"]
     },
     "Bag1": {
         "name": "Bag1",
         "description": "Carry your essentials in style with our chic and functional bag.",
         "price": "$115.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\bag1.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\bag1.png",
         "recommended": ["Bag2", "Bag3", "Bag4"]
     },
     "Bag2": {
         "name": "Bag2",
         "description": "Carry your essentials in style with our chic and functional bag.",
         "price": "$115.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\bag2.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\bag2.png",
         "recommended": ["Bag1", "Bag3", "Bag4"]
     },
     "Bag3": {
         "name": "Bag3",
         "description": "Carry your essentials in style with our chic and functional bag.",
         "price": "$115.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\bag3.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\bag3.png",
         "recommended": ["Bag1", "Bag2", "Bag4"]
     },
     "Bag4": {
         "name": "Bag4",
         "description": "Carry your essentials in style with our chic and functional bag.",
         "price": "$115.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\bag4.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\bag4.png",
         "recommended": ["Bag1", "Bag2", "Bag3"]
     },
     "Boot1": {
         "name": "Boot1",
         "description": "Stay cozy and stylish this season with our fashionable and comfortable boots.",
         "price": "$150.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\boots1.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\boots1.png",
         "recommended": ["Boot2", "Boot3", "Boot4"]
     },
     "Boot2": {
         "name": "Boot2",
         "description": "Stay cozy and stylish this season with our fashionable and comfortable boots.",
         "price": "$150.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\boots2.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\boots2.png",
         "recommended": ["Boot1", "Boot3", "Boot4"]
     },
     "Boot3": {
         "name": "Boot3",
         "description": "Stay cozy and stylish this season with our fashionable and comfortable boots.",
         "price": "$150.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\boots3.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\boots3.png",
         "recommended": ["Boot1", "Boot2", "Boot4"]
     },
     "Boot4": {
         "name": "Boot4",
         "description": "Stay cozy and stylish this season with our fashionable and comfortable boots.",
         "price": "$150.00",
-        "image": "C:\Users\ramin\Downloads\yo (2)\clothes\boots4.png",
+        "image": r"C:\Users\ramin\Downloads\yo (2)\clothes\boots4.png",
         "recommended": ["Boot1", "Boot2", "Boot3"]
     }
 }
